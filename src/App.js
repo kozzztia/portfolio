c8609815257cf40dc1728a62cb5bc7f34a6b8520
Kozzztia <kozzztia@ukr.net>
2022-06-11 23:43:41 +0300

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react'
 
 function App() {
+
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+
+  const [number, setNumber] = useState("");
+  const addCash = (number) => {
+    dispatch({ type: 'ADD_CASH', payload: number });
+  }
+  const getCash = (number) => {
+    dispatch({ type: 'GET_CASH', payload: number });
+  }
+  const resetCash = (number) => {
+    dispatch({ type: 'RESET_CASH', payload: number = 0 })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>{cash}</h2>
+      </div>
+      <div>
+        <input style={{ 'padding': '10px' }}
+          type="number"
+          onChange={(e) => setNumber(e.target.value)}
+          onBlur={(e) => setTimeout(() => { setNumber(e.target.value = "") }, 100)}
+        />
+        <button style={{ 'padding': '10px' }}
+          onClick={() => addCash(number)}
+        >+</button>
+        <button style={{ 'padding': '10px' }}
+          onClick={() => getCash(number)}
+        >-</button>
+        <button style={{ 'padding': '10px' }}
+          onClick={() => resetCash()}
+        >RESET</button>
+      </div>
+
     </div>
   );
 }
